df5defa833f0e41907fd05ff0ed70782af339c0e
11328476-DCIT205 <paakofi.one@gmail.com>
2025-08-04 17:07:05 +0000

    grading system

--- assignment1/gradecalculator/Program.cs
+++ assignment1/gradecalculator/Program.cs
@@ -2,3 +2,23 @@
 int gradeInput = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Your grade is {gradeInput} out of 100");
 
+if (gradeInput >= 90)
+{
+    Console.WriteLine("Your grade is an A");
+}
+else if (gradeInput >= 80 && gradeInput <= 89)
+{
+    Console.WriteLine("Your grade is a B");
+}
+else if (gradeInput >= 70 && gradeInput <= 79)
+{
+    Console.WriteLine("Your grade is a C");
+}
+else if (gradeInput >= 60 && gradeInput <= 69)
+{
+    Console.WriteLine("Your Grade is a D");
+}
+else if (gradeInput < 60);
+{
+    Console.WriteLine("Your grade is an F");
+}
