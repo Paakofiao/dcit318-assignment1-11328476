6626258efbbdecb4d01fd86bb173e45920ba2ad7
11328476-DCIT205 <paakofi.one@gmail.com>
2025-08-04 19:23:19 +0000

    triangle identification

--- assignment1/TriangleTypeidentifier/Program.cs
+++ assignment1/TriangleTypeidentifier/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("First Side");
 int sideOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Second Side");
+int sideTwo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Third Side");
+int sideThree = Convert.ToInt32(Console.ReadLine());
+
+
+if (sideOne == sideTwo || sideTwo == sideThree || sideOne==sideThree)
+{
+    Console.WriteLine("The triangle  is Isosceles");
+}
+else if (sideOne == sideTwo && sideTwo == sideThree)
+{
+    Console.WriteLine("The triangle is equilateral");
+} 
+else  {
+    Console.WriteLine("The Triangle is Scalene");
+}
